bae7443e85e445ee4e0c8759f354e3c4650cdc4f
Mohammad Mohsen <m.m.asemanmanzar@gmail.com>
2023-12-17 19:54:46 +0330

    add retry

--- add-ranking.js
+++ add-ranking.js
@@ -38,9 +38,17 @@ async function getMovieId(movieNameKey) {
             console.log("tubi-firefox-addon: error2:", href, groups)
         }
     }
-    console.log("tubi-firefox-addon: error3:", url, text)
+    if (text.includes("Unfortunately, bots use DuckDuckGo too")) {
+        console.log("tubi-firefox-addon: DuckDuckGo rate limit for", url)
+    } else {
+        console.log("tubi-firefox-addon: error3:", url, text)
+    }
     return undefined;
 }
+
+function sleep(ms) { return new Promise(r => setTimeout(r, ms)) };
+
+function getRandomInt(max) { return Math.floor(Math.random() * max); }
 
 async function getRanking(movieName, movieYear) {
     let movieNameKey = getKey(movieName, movieYear);
@@ -69,12 +77,27 @@ async function getRanking(movieName, movieYear) {
     }
 }
 
-function injectRanking(titleElement, movieData) {
+function injectLoading(title) {
+    removeExistingRakingFromElement(title.parentElement)
     const div = document.createElement("div");
-    div.className = "web-rating tubi-add-ranking";
+    div.className = "web-rating tubi-firefox-addon";
     div.style.whiteSpace = "pre";
 
-    if (movieData.foundImdbRating) {
+    div.style.backgroundColor = "gray";
+    div.style.color = "white";
+    div.style.fontStyle = "italic";
+
+    div.innerHTML = "loading ...";
+    title.titleElement.after(div);
+}
+
+function injectRanking(title, movieData) {
+    if (movieData && movieData.foundImdbRating) {
+        removeExistingRakingFromElement(title.parentElement)
+        const div = document.createElement("div");
+        div.className = "web-rating tubi-firefox-addon";
+        div.style.whiteSpace = "pre";
+
         let r = parseFloat(movieData.imdbRating)
         if (isNaN(r)) {
             div.style.backgroundColor = "gray";
@@ -95,18 +118,78 @@ function injectRanking(titleElement, movieData) {
             e.preventDefault();
             window.open(`https://www.imdb.com/title/${movieData.movieId}/`);
         };
-        titleElement.after(div);
+        title.titleElement.after(div);
     }
-
 }
 
-function main() {
-    let oldRankings = document.querySelectorAll(".tubi-add-ranking");
+async function getAndInjectRanking(title) {
+    let movieData = await getRanking(title.movieName, title.movieYear);
+    if (movieData) {
+        injectRanking(title, movieData)
+        return { success: true }
+    } else {
+        return { success: false, title }
+    }
+}
+
+
+async function runTasks(titles) {
+    // add loading
+    for (let i = 0; i < titles.length; i++) {
+        const title = titles[i];
+        injectLoading(title);
+    }
+
+    // fast retrieval + add cached ones
+    let promises = [];
+    for (let i = 0; i < titles.length; i++) {
+        const title = titles[i];
+        promises.push(getAndInjectRanking(title));
+    }
+    let results = await Promise.all(promises);
+    let failedTitles = [];
+    for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        if (result.success !== true) {
+            let t = result.title;
+            t.tryCount = 1;
+            failedTitles.push(t);
+        }
+    }
+
+    // one-by-one retrieval
+    if (failedTitles.length > 0) {
+        console.log("tubi-firefox-addon: one-by-one retrieval after 5s sleep");
+        await sleep(5000);
+    }
+    while (failedTitles.length > 0) {
+        const title = failedTitles.pop();
+        await sleep((getRandomInt(1000) + 500) * title.tryCount);
+        let result = await getAndInjectRanking(title);
+        if (result.success !== true) {
+            title.tryCount += 1;
+            // try again later
+            if (title.tryCount <= 3) {
+                failedTitles.push(title);
+            } else {
+                removeExistingRakingFromElement(title.parentElement);
+            }
+        }
+    }
+
+    console.log("tubi-firefox-addon: done");
+}
+
+function removeExistingRakingFromElement(parent) {
+    let oldRankings = parent.querySelectorAll(".tubi-firefox-addon");
     for (let i = 0; i < oldRankings.length; i++) {
         const element = oldRankings[i];
         element.remove();
     }
+}
 
+function main() {
+    let titles = [];
     let parentElements = document.getElementsByClassName('web-content-tile__content-digest');
     for (let i = 0; i < parentElements.length; ++i) {
         let parentElement = parentElements[i];
@@ -114,11 +197,13 @@ function main() {
             let titleElement = parentElement.getElementsByClassName("web-content-tile__title")[0];
             let movieName = titleElement.innerHTML;
             let movieYear = parentElement.getElementsByClassName("web-content-tile__year")[0].innerHTML;
-            getRanking(movieName, movieYear).then((movieData) => { injectRanking(titleElement, movieData) })
+            titles.push({ movieName, movieYear, titleElement, parentElement })
         } catch (error) {
             console.error("error in parsing element", error, parentElement)
         }
     }
+    runTasks(titles);
 }
 
+// browser.storage.local.clear();
 main()
